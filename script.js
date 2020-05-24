8e033b764d40396f79c4266ac8028993b40f9807
Iruhdeg7 <65547304+Iruhdeg7@users.noreply.github.com>
2020-05-24 11:57:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function posting() {
     document.getElementById("post").appendChild(titre);
     document.getElementById("post").appendChild(contenant);
 };
+
+function calcul() {
+    var prix = document.getElementById("prix").value;
+    var taux = document.getElementById("taux").value;
+    var op = document.getElementById("op").value;
+
+    var benefice = prix * taux / 100;
+
+    if (op === "+") {
+        var prixFinal = prix * (1 + taux / 100);
+        document.getElementById("final").innerHTML = "Le prix après l'augmentation est de : " + prixFinal;
+        document.getElementById("benefice").innerHTML = "Le taux de benefice est de : " + benefice;
+    } else if (op === "-") {
+        var prixFinal = prix * (1 - taux / 100);
+        document.getElementById("final").innerHTML = "Le prix après la remise est de : " + prixFinal;
+        document.getElementById("benefice").innerHTML = "Le taux de benefice est de : " + benefice;
+    } else {
+        document.getElementById("final").innerHTML = "Veuillez Entrer soit le signe '+' ou '-', Merci";
+
+    }
+}
